9e16584fbfb7999f8de1f46ae040060f0515cafb
unexpectedd0wn <unexpectedd0wn@gmail.com>
2023-05-02 14:38:20 +0200

    #configure files for the project

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,32 @@
 const { defineConfig } = require("cypress");
+const sqlServer = require('cypress-sql-server');
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    requestTimeout: 7000,
+    e2e: {
+      setupNodeEvents(on, config) {
+        const tasks = sqlServer.loadDBPlugin(config.env.db);
+        on("task",tasks)
+        on('task',{queryDb:query=>{return queryTestDb(query)},});
+        on('task', { queryDb: query => { return queryTestDb(query, config) }, }); //For running sql query
+      },
+      testIsolation: false,
+    env: {
+      
+      devURL: "https://************.com",
+      db: {
+        userName: "************",
+        password: "************",
+        server: "************",
+        options: {
+          database: "************",
+          encrypt: true,
+          rowCollectionOnRequestCompletion: true,
+          //port: '1433',
+          //timeout:70000
+          // Default Port
+        },
+      },
     },
   },
 });
